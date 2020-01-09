2c1b379abe86c3d6fd6b28054fbd96228c6235eb
elsonlim-tw <elsonlim.thoughtworks.com>
2020-01-09 14:16:17 +0800

    A simple setup of React app using Webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require("path");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index_bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
